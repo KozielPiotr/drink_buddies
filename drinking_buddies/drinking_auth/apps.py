07f09802b925c9508928c93da8328c344fb5c378
Piotr Kozieł <p.koziel@gidcoders.pl>
2021-04-08 21:05:59 +0200

    prettify code

--- drinking_buddies/drinking_auth/apps.py
+++ drinking_buddies/drinking_auth/apps.py
@@ -1,6 +1,10 @@
+"""Configuration for drinking_auth application."""
+
 from django.apps import AppConfig
 
 
 class DrinkingAuthConfig(AppConfig):
+    """Basic app config."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "drinking_buddies.drinking_auth"
